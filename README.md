78bd33b5239e1cd7b9abd31827b059c0f2800d07
John A Maloney <john.a.maloney@gmail.com>
2018-10-03 21:42:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# @distantbluesoftware/dbsutil
+
+A bunch of helper utilities that probably exist elsewhere, but also exist here. 
+
+## Install
+
+`yarn add @distantbluesoftware/dbsutil`
+
+## Usage 
+
+`import dbsutil from 'dbsutil';`
+
+### String.removeWhitespace
+
+Removes all white space from a string. 
+
---
+++ index.js
@@ -0,0 +1,37 @@
+(
+  function() {
+    let utils = {};
+    
+    utils.removeWhitespace = () => {
+      return this.replace(/\s/g,'');
+    }
+    
+    utils.slugify = string => {
+      return string
+        .toString()
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+    }
+
+    utils.iterator = (arr, field, comp, aggregator = 0) => {
+      arr.forEach(p => {
+        if (p[field] === comp) {
+        aggregator++
+        comp = comp + '-' + aggregator
+        iterator(arr, field, comp, aggregator)
+        }
+      })
+      return aggregator
+    }
+
+    utils.pad = (number, digits) => {
+        return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+    }
+    
+  }()
+);
