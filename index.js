4015a732721a30803bba7e8182a0fc73d30a4f04
John A Maloney <john.a.maloney@gmail.com>
2018-10-03 22:17:51 -0500

    validateEmails

--- index.js
+++ index.js
@@ -3,19 +3,24 @@
     let utils = {};
     
     if (typeof exports != 'undefined' && !exports.nodeType) {
-    if (typeof module != 'undefined' && !module.nodeType && module.exports) {
-      exports = module.exports = utils;
+      if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+        exports = module.exports = utils;
+      }
+      exports.utils = utils;
+    } else {
+      root.utils = utils;
     }
-    exports.utils = utils;
-  } else {
-    root.utils = utils;
-  }
     
     utils.removeWhitespace = string => {
       if (typeof string !== 'string') {
         throw new TypeError(`String is expected, got ${typeof string}`);
       }
       return string.replace(/\s/g,'');
+    }
+    
+    utils.validateEmail = email => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
     }
     
     utils.slugify = string => {
